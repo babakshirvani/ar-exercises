cd0d7e3a06da7f44de9b9b6ac1cd7f2c472d2488
Babak Shirvani <bk.shirvani@gmail.com>
2021-10-12 02:47:22 +0000

    complete ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total Revenue: #{Store.sum(:annual_revenue)}"   #Total Revenue: 4114000
+puts "Average Revenue: #{Store.average(:annual_revenue)}"  #Average Revenue: 822800.0
+
+puts "The best Stores: #{Store.where("annual_revenue <= 1000000").count}"
